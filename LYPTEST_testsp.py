9e586002a9626e357d889032ce9f675e1087ea6a
lyp830414 <lyp830414@163.com>
2017-10-15 17:07:49 +0800

    fix one issue

--- LYPTEST_testsp.py
+++ LYPTEST_testsp.py
@@ -4,33 +4,20 @@ from splinter.browser import Browser
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 
-
-def start_firefox_with_firebug_plug():
-    """启动Firefox，并自动加载插件Firebug"""
-    firefoxBin = os.path.abspath(r"C:\Program Files (x86)\Mozilla Firefox\firefox.exe")
-    os.environ["webdriver.firefox.bin"] = firefoxBin
-
-    firefoxProfile = webdriver.FirefoxProfile()
-    tempDir = os.getcwd()
-    tempDir = os.path.split(tempDir)[0]
-    firebugPlugFile = os.path.join(r"C:\Users\Administrator\AppData\Roaming\Mozilla\Firefox\Profiles\82bh7hqw.default\extensions", "cehomepage@mozillaonline.com")
-    firefoxProfile.add_extension(firebugPlugFile)
-    firefoxProfile.set_preference("extensions.firebug.currentVersion", "2.0.7")
-
-    driver = webdriver.Firefox(firefox_profile=firefoxProfile)
-    driver.get("https://api.weibo.com/oauth2/authorize?client_id=3054804248&redirect_uri=http%3A%2F%2Fwww.huajiao.com&response_type=code&state=eyJzb3VyY2UiOiJzaW5hIiwicmVkaXJlY3QiOiJodHRwOlwvXC93d3cuaHVhamlhby5jb21cLz9ocmQ0Nzk1IiwidXNlcl9yYW5kIjoiNDY5YWM3NDUzNWY2MjkwMDdhOTZiYjcyNDExMGM2MjciLCJiYW5qdW1wIjoiIn0%3D")
-
 def splinter2(browser):
-        browser.find_element_by_id("userId").clear()
-        browser.find_element_by_id("userId").send_keys("xiangyuzhenniao")
-        browser.find_element_by_id("passwd").clear()
-        browser.find_element_by_id('passwd').send_keys('XyznSsz1983@')
+    browser.find_element_by_id("userId").clear()
+    browser.find_element_by_id("userId").send_keys("name")
+    browser.find_element_by_id("passwd").clear()
+    browser.find_element_by_id('passwd').send_keys('password')
 
+    i = 0
+    while i < 5:
         contents = browser.find_element_by_class_name("WB_btn_login")
         if contents:
             contents.click()
+        i = i+1
 
-def splinterWeibo(url):
+def splinter(url):
 
     firefoxProfile = FirefoxProfile()
     firefoxProfile.set_preference('plugin.state.flash', '2')
@@ -49,19 +36,7 @@ def splinterWeibo(url):
         contents2 = browser.find_element_by_class_name("sina")
         if contents2:
             contents2.click()
-
-            websize3 = 'https://api.weibo.com/oauth2/authorize?client_id=3054804248&redirect_uri=http%3A%2F%2Fwww.huajiao.com&response_type=code&state=eyJzb3VyY2UiOiJzaW5hIiwicmVkaXJlY3QiOiJodHRwOlwvXC93d3cuaHVhamlhby5jb21cLz9ocmQ0Nzk1IiwidXNlcl9yYW5kIjoiNDY5YWM3NDUzNWY2MjkwMDdhOTZiYjcyNDExMGM2MjciLCJiYW5qdW1wIjoiIn0%3D'
             splinter2(browser)
-
-    firefoxProfile = webdriver.FirefoxProfile()
-    tempDir = os.getcwd()
-    tempDir = os.path.split(tempDir)[0]
-    firebugPlugFile = os.path.join(r"C:\Users\Administrator\AppData\Roaming\Mozilla\Firefox\Profiles\82bh7hqw.default\extensions", "cehomepage@mozillaonline.com")
-    firefoxProfile.add_extension(firebugPlugFile)
-    firefoxProfile.set_preference("extensions.firebug.currentVersion", "2.0.7")
-
-    driver = webdriver.Firefox(firefox_profile=firefoxProfile)
-    driver.get("https://api.weibo.com/oauth2/authorize?client_id=3054804248&redirect_uri=http%3A%2F%2Fwww.huajiao.com&response_type=code&state=eyJzb3VyY2UiOiJzaW5hIiwicmVkaXJlY3QiOiJodHRwOlwvXC93d3cuaHVhamlhby5jb21cLz9ocmQ0Nzk1IiwidXNlcl9yYW5kIjoiNDY5YWM3NDUzNWY2MjkwMDdhOTZiYjcyNDExMGM2MjciLCJiYW5qdW1wIjoiIn0%3D")
 
 def splinterQQ(url):
     firefoxProfile = FirefoxProfile()
@@ -132,7 +107,7 @@ def splinterOld(url):
     browser.find_element_by_id("userId").clear()
     browser.find_element_by_id("userId").send_keys("xiangyuzhenniao")
     browser.find_element_by_id("passwd").clear()
-    browser.find_element_by_id('passwd').send_keys('ilovekorea')
+    browser.find_element_by_id('passwd').send_keys('XyznSsz1983@')
 
     #browser.find_by_xpath('//*[@class="WB_btn_login formbtn_01"]/a').first.click()
 
@@ -144,4 +119,4 @@ def splinterOld(url):
 if __name__ == '__main__':
     websize3 = 'http://www.huajiao.com/'
     #splinterWeibo(websize3)
-    splinterQQ(websize3)
+    splinter(websize3)
